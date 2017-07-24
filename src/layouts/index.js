816307b6a9f10075bcaa4721cf5c4a1790bab4fa
JohannBillar <jwbillar@gmail.com>
2017-07-23 20:45:21 -0700

    Start adding navigation

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,61 +1,60 @@
-import React from "react"
-import PropTypes from "prop-types"
-import Link from "gatsby-link"
-import Helmet from "react-helmet"
-import { rhythm } from "../utils/typography"
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'gatsby-link';
+import Helmet from 'react-helmet';
+import { rhythm } from '../utils/typography';
+import Navigation from '../components/Navigation/Navigation';
 
 export default class Template extends React.Component {
   static propTypes = {
-    children: PropTypes.func,
-  }
+    children: PropTypes.func
+  };
 
   render() {
     return (
       <div>
         <Helmet
-          title="Gatsby Default Starter"
-          meta={[
-            { name: "description", content: "Sample" },
-            { name: "keywords", content: "sample, something" },
-          ]}
+          title="Quality Link Consulting"
+          meta={[{ name: 'description', content: 'Sample' }, { name: 'keywords', content: 'sample, something' }]}
         />
         <div
           style={{
             background: `rebeccapurple`,
-            marginBottom: rhythm(1),
+            marginBottom: rhythm(1)
           }}
         >
           <div
             style={{
               margin: `0 auto`,
               maxWidth: 960,
-              padding: `${rhythm(1)} ${rhythm(3 / 4)}`,
+              padding: `${rhythm(1)} ${rhythm(3 / 4)}`
             }}
           >
             <h1 style={{ margin: 0 }}>
               <Link
                 to="/"
                 style={{
-                  color: "white",
-                  textDecoration: "none",
+                  color: 'white',
+                  textDecoration: 'none'
                 }}
               >
-                Gatsby
+                Quality Link
               </Link>
             </h1>
           </div>
         </div>
+        <Navigation />
         <div
           style={{
             margin: `0 auto`,
             maxWidth: 960,
             padding: `${rhythm(1)} ${rhythm(3 / 4)}`,
-            paddingTop: 0,
+            paddingTop: 0
           }}
         >
           {this.props.children()}
         </div>
       </div>
-    )
+    );
   }
 }
