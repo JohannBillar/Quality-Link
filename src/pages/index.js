93b21db02dc79506497d1ddcf26b8c2efd6b6f9a
JohannBillar <jwbillar@gmail.com>
2017-10-03 13:20:58 -0700

    Create header text components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
 import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
+import { Header1, Header2, Header3 } from '../components/text/Headers';
 
 export default class Index extends Component {
   render() {
     return (
-      <section>
-        <h1>Consulting</h1>
-        <h2>Audits and Consulting</h2>
-        <h3>Providing Global Quality Assurance Consulting and Auditing for the GxP regulated industry</h3>
-        <p>
-          Quality Link Consulting offers a range of services to provide tailored compliance solutions to meet the needs
-          of the biotechnology, pharmaceutical and medical device, and supporting industries.
-        </p>
-      </section>
+      <div>
+        <section>
+          <div>
+            <Header1 color="#161f4b">Consulting</Header1>
+          </div>
+        </section>
+
+        <Header2 color="#161f4b">Audits and Consulting</Header2>
+        <section className="row">
+          <div className="seven columns">
+            <Header3 color="#3348b1">
+              Providing Global Quality Assurance Consulting and Auditing for the GxP regulated industry
+            </Header3>
+            <p>
+              Quality Link Consulting offers a range of services to provide tailored compliance solutions to meet the
+              needs of the biotechnology, pharmaceutical and medical device, and supporting industries.
+            </p>
+          </div>
+          <aside className="five columns">
+            <Header3 color="#3348b1">Specialization In</Header3>
+            <ul class="quality-link-icon">
+              <li>GCP and GCLP Audits</li>
+              <li>Investigations and For-Cause Audits</li>
+              <li>Inspection Preparation and Management</li>
+              <li>Quality Management Systems</li>
+              <li>Special Populations/Orphan Diseases</li>
+              <li>Vendor Management</li>
+            </ul>
+          </aside>
+        </section>
+      </div>
     );
   }
 }
