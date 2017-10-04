330259147c89cd1f6b2bcd9cc404fc782948ff85
JohannBillar <jwbillar@gmail.com>
2017-10-04 15:58:23 -0700

    Delete deprecated code from index.js

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -18,34 +18,6 @@ export default class Template extends React.Component {
           title="Quality Link Consulting"
           meta={[{ name: 'description', content: 'Sample' }, { name: 'keywords', content: 'sample, something' }]}
         />
-        {/* 
-          <div
-            style={{
-              background: `#1d2965`,
-              marginBottom: rhythm(1)
-            }}
-          >
-            <div
-              style={{
-                margin: `0 auto`,
-                maxWidth: 960,
-                padding: `${rhythm(1)} ${rhythm(3 / 4)}`
-              }}
-            >
-              <h1 style={{ margin: 0 }}>
-                <Link
-                  to="/"
-                  style={{
-                    color: 'white',
-                    textDecoration: 'none'
-                  }}
-                >
-                  Quality Link
-                </Link>
-              </h1>
-            </div>
-          </div>
-        */}
         <Navigation />
         <div
           style={{
