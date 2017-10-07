4c2cbb7a1942d5495f3386929f165ad127459c11
JohannBillar <jwbillar@gmail.com>
2017-10-07 13:14:13 -0700

    Renamed footer and navigation

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
 import { rhythm } from '../utils/typography';
-import Navigation from '../components/navigation/Navigation';
-import Footer from '../components/footer/Footer';
+import Navigation from '../components/navigation/navigation';
+import Footer from '../components/footer/footer';
 import '../css/main.css';
 
 export default class Template extends React.Component {
