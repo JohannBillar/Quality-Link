2ab4d0961c43e250dce125dbcc029405cee1f179
JohannBillar <jwbillar@gmail.com>
2017-10-07 14:18:15 -0700

    Fix path to navigation component

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
 import { rhythm } from '../utils/typography';
-import Navigation from '../components/navigation/navigation';
+import Navigation from '../components/Navigation/navigation';
 import Footer from '../components/footer/footer';
 import '../css/main.css';
 
