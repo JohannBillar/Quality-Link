d03bcf35b80eefc072ac48406f0b163152c09216
JohannBillar <jwbillar@gmail.com>
2017-10-08 17:35:20 -0700

    Customize thanks page

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import Link from 'gatsby-link';
+import { rhythm } from '../utils/typography';
+
+const container = {
+  margin: `0 auto`,
+  maxWidth: 960,
+  padding: `${rhythm(1)} ${rhythm(3 / 4)}`,
+  paddingTop: 0
+};
 
 export default () => (
-  <div>
-    <h1>Thank you!</h1>
-    <p>This is a custom thank you page for submissions from the contact form</p>
+  <div style={container}>
+    <div className="row">
+      <section className="six columns services" style={{ marginTop: `${rhythm(3)}` }}>
+        <Link to="/">
+          <div className="quality-link-icon-xxl" />
+        </Link>
+      </section>
+      <div className="six columns" style={{ marginTop: `${rhythm(3)}` }}>
+        <h1 className="mobile-h1">Thank you!</h1>
+        <p>We will get back to you as soon as possilble</p>
+      </div>
+    </div>
   </div>
 );
