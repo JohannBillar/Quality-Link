f29d6959ad8cac7319a9bf2e2d66d6f4ab2a6fb7
JohannBillar <jwbillar@gmail.com>
2017-10-10 08:04:12 -0700

    Update gatsby, link and add sitemap plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Quality Link Consulting`
+    title: `Quality Link`,
+    siteUrl: `https://http://localhost:8000/`
   },
-  plugins: [`gatsby-plugin-react-helmet`]
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-sitemap`
+    }
+  ]
 };
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -16,7 +16,17 @@ export default class Template extends React.Component {
       <main>
         <Helmet
           title="Quality Link Consulting"
-          meta={[{ name: 'description', content: 'Sample' }, { name: 'keywords', content: 'sample, something' }]}
+          meta={[
+            {
+              name: 'GCP and GCLP Audits',
+              content: 'Providing Global Quality Assurance Consulting and Auditing for the GxP regulated industry'
+            },
+            {
+              name: 'Global Quality Audits',
+              content:
+                'GCP Site: audits of clinical investigator sites for compliance with the clinical protocol, ICH GCPs, and applicable country regulations'
+            }
+          ]}
         />
         <Navigation />
         <div>{this.props.children()}</div>
